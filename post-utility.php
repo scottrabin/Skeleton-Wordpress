1b17b0fe0dc887c9af67597a7ff97487f9c77e84
Scott Rabin <scottrabin@gmail.com>
2012-01-29 13:07:41 -0500

    Adjusted post utility generation for easier styling; also stripped extra characters

--- post-utility.php
+++ post-utility.php
@@ -1,9 +1,30 @@
 <div class="post-utility">
-   <span class="cat-links">
-   <span class="entry-utility-prep entry-utility-prep-cat-links"><?php _e( 'Posted in ', WP_THEME_NAME ); ?></span>
- <?php echo get_the_category_list(', '); ?>
-   </span>
-   <span class="meta-sep"> | </span>
-   <?php the_tags( '<span class="tag-links"><span class="entry-utility-prep entry-utility-prep-tag-links">' . __('Tagged ', WP_THEME_NAME ) . '</span>', ", ", "</span>\n\t\t\t\t\t\t<span class=\"meta-sep\">|</span>\n" ) ?>
+<?php
+
+   $utilities = array();
+
+$categories = get_the_category_list(', ');
+if( strlen($categories) > 0 ){
+  $utilities[] = swp_generate_html( array(
+										  'tagName' => 'span',
+										  'class' => 'cat-links',
+										  'contents' => __( 'Posted in ', WP_THEME_NAME ) . $categories
+										  ));
+}
+
+$tags = get_the_tags();
+if( is_array($tags) && sizeof($tags) > 0 ){
+  foreach($tags as $index => $tag ){
+	$tags[$index] = sprintf( '<a href="%s">%s</a>', get_tag_link( $tag->term_id ), $tag->name );
+  }
+  $utilities[] = swp_generate_html( array(
+										  'tagName' => 'span',
+										  'class' => 'tag-links',
+										  'contents' => __('Tagged ', WP_THEME_NAME ) . implode( ', ', $tags )
+										  ));
+}
+
+print implode('<span class="meta-sep"> | </span>', $utilities);
+?>
    <span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', WP_THEME_NAME ), __( '1 Comment', WP_THEME_NAME ), __( '% Comments', WP_THEME_NAME ) ) ?></span>
 </div><!-- .post-utility -->
