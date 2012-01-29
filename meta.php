feb4e39d42f241ed79f984a1b29faf9610e50b90
Scott Rabin <scottrabin@gmail.com>
2012-01-29 12:21:30 -0500

    Adjusted 'Continue Reading' link and floated images relative to post utility space

--- meta.php
+++ meta.php
@@ -3,10 +3,9 @@
    <span class="author vcard">
    <a class="url fn n" href="<?php echo get_author_link( false, $authordata->ID, $authordata->user_nicename ); ?>" title="<?php printf( __( 'View all posts by %s', WP_THEME_NAME ), $authordata->display_name ); ?>"><?php the_author(); ?></a>
    </span>
-   <span class="meta-sep"> | </span>
-   <span class="meta-prep meta-prep-entry-date"><?php _e('Published ', WP_THEME_NAME); ?></span>
-   <span class="entry-date">
+   <span class="meta-prep meta-prep-entry-date"><?php _e('on ', WP_THEME_NAME); ?></span>
+   <span class="post-date">
    <abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php the_time( get_option( 'date_format' ) ); ?></abbr>
    </span>
- <?php edit_post_link( __( 'Edit', WP_THEME_NAME ), "<span class=\"meta-sep\">|</span>\n\t\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t" ) ?>
-</div><!-- .entry-meta -->
+ <?php edit_post_link( __( 'Edit', WP_THEME_NAME ), "<span class=\"edit-link\">", "</span>" ) ?>
+</div><!-- .post-meta -->
