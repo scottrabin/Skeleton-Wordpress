443f1c1cadcd3792464cd6da8c50c237db6afdd4
Scott Rabin <scottrabin@gmail.com>
2012-01-29 14:34:19 -0500

    Fixed render error in theme options

--- settings/templates/option-text.php
+++ settings/templates/option-text.php
@@ -3,6 +3,6 @@
 $options[$id] = stripslashes($options[$id]);  
 $options[$id] = esc_attr( $options[$id]);  
 echo "<input class='regular-text$field_class' type='text' id='$id' name='swp_options[$id]' value='$options[$id]' />";  
-echo ($desc != '') ? "<br />&lt;span class='description'>$desc</span>" : "";  
+echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";  
         
 ?>
