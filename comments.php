f9b481028fb2dbaf779c105983034b36fd6e53c0
Scott Rabin <scottrabin@gmail.com>
2012-01-30 23:33:01 -0500

    Adjusted bottom navigation after moving comment field below comment list; added in-page link to comment form before comment list

--- comments.php
+++ comments.php
@@ -1,21 +1,14 @@
-<?php if ( comments_open() ) : ?>
+<?php if( comments_open() ): ?>
+<a href="#respond" rel="nofollow" title="Leave a comment">Leave a comment</a> | 
+   <?php endif; ?>
 
-<?php /* comment replies */ wp_enqueue_script( 'comment-reply' ); ?>
- <?php
+   <span><?php printf( _n( '%s response', '%s responses', get_comments_number() ), number_format_i18n( get_comments_number() ) ); ?></span>
 
-$comment_form_options = array( 'comment_notes_before' => '',
-							   'comment_notes_after' => '');
-
-
- comment_form( $comment_form_options );
- ?>
-
-<?php endif; ?>
+ <?php if( comments_open() ): ?>
+<a href="<?php print comments_rss(); ?>" class="icon alignright theme-color-hover" type="application/rss+xml" rel="alternate" title="Comments RSS for <?php the_title_attribute(); ?>">B</a>
+   <?php endif; ?>
 
 <?php if ( have_comments() ) : ?>
-	<h3 id="comments"><?php	printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number() ),
-									number_format_i18n( get_comments_number() ), '&#8220;' . get_the_title() . '&#8221;' ); ?><a href="<?php print comments_rss(); ?>" class="icon alignright theme-color-hover" type="application/rss+xml" rel="alternate" title="Comments RSS for <?php the_title_attribute(); ?>">B</a></h3>
-
 	<div class="navigation">
 		<div class="alignleft"><?php previous_comments_link() ?></div>
 		<div class="alignright"><?php next_comments_link() ?></div>
@@ -32,11 +25,24 @@ $comment_form_options = array( 'comment_notes_before' => '',
  <?php else : // this is displayed if there are no comments so far ?>
 
 	<?php if ( comments_open() ) : ?>
-		<!-- If comments are open, but there are no comments. -->
+	   <?php //<!-- If comments are open, but there are no comments. --> ?>
 
 	 <?php else : // comments are closed ?>
-		<!-- If comments are closed. -->
+	   <?php // <!-- If comments are closed. -->?>
 		<p class="nocomments"><?php _e('Comments are closed.'); ?></p>
 
 	<?php endif; ?>
+<?php endif; ?>
+<?php if ( comments_open() ) : ?>
+
+<?php /* comment replies */ wp_enqueue_script( 'comment-reply' ); ?>
+ <?php
+
+$comment_form_options = array( 'comment_notes_before' => '',
+							   'comment_notes_after' => '');
+
+
+ comment_form( $comment_form_options );
+ ?>
+
 <?php endif; ?>
