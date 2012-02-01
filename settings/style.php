e0e9e2885e295896311bec5dd76d56bf51b0b503
Scott Rabin <scottrabin@gmail.com>
2012-01-31 21:32:36 -0500

    Removed unnecessary files

--- settings/style.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$sidebar_position = swp_get_option('sidebar_position');
-$content_position = ( $sidebar_position === 'left' ? 'right' : 'left' );
-
-?>
-
-#content{ float: <?php print $content_position; ?>; }
-#sidebar{ float: <?php print $sidebar_position; ?>; }
